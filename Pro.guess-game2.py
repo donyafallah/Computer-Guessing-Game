b7e8891ea97b76defb1dd9b72fe121f45965de98
donya <donyafallah2004@gmail.com>
2024-07-10 13:15:06 +0330

    Clarification was provided in English!

--- Pro.guess-game2.py
+++ Pro.guess-game2.py
@@ -3,13 +3,15 @@ a = 1
 b = 99
 computer_guess = random.randint(a,b)
 print(computer_guess)
-user_idea = input()
-while user_idea != "d" :
-    if user_idea == "b":
+user_idea = input("Enter one of these option \t<< 1)bigger \t2)smaller \t3)true >> : ")
+while user_idea != "true" :
+    if user_idea == "bigger":
         a = computer_guess + 1
-    elif user_idea == "k" :
+    elif user_idea == "smaller" :
         b = computer_guess - 1
+    else :
+        print("Just choose one of them {not choose anything else!} \t<< 1)bigger \t2)smaller \t3)true >> : ")    
     computer_guess = random.randint(a,b)    
     print(computer_guess)
-    user_idea = input()
-               
+    user_idea = input("Enter one of these option \t<< 1)bigger \t2)smaller \t3)true >> : ")
+print("Congratulations dear Computer!")               
